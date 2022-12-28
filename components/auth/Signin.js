58b2a3c6815337a33ff941ebc2b1b5c96c6bc672
sisirdhakal <dhakalsaurav023@gmail.com>
2022-12-28 16:02:27 +0545

    login almost done

--- components/auth/Signin.js
+++ components/auth/Signin.js
@@ -25,14 +25,27 @@ function Signin() {
   return (
     <>
       <div className='flex justify-center items-center h-full'>
-        <div className='grid grid-cols-2 gap-16 w-full hover:shadow-xl shadow-black transition-all ease-linear duration-300 lg:w-[800px] px-2 lg:px-8 rounded-md py-5 mx-auto bg-white'>
-          <div>
-            <div className='relative w-full mx-auto h-[280px]  rounded-sm'>
+        <div className='grid grid-cols-2 gap-8 w-full hover:shadow-xl shadow-black transition-all ease-linear duration-300 lg:w-[800px] px-2 lg:px-6 rounded py-5 mx-auto bg-white'>
+          <div className=' gap-1 px-2 py-10'>
+            
+            <div className='relative w-[240px] mx-auto h-[120px]  rounded-sm'>
+              <Image
+                alt=''
+                priority
+                src={"/assets/images/wrcms.svg"}
+                className='rounded-md object-cover'
+                fill
+                sizes="(min-width: 60em) 24vw,
+                            (min-width: 28em) 45vw,
+                            100vw"
+              />
+            </div>
+            <div className='relative w-full mx-auto h-[240px]  rounded-sm'>
               <Image
                 alt=''
                 priority
                 src={"/assets/images/wrc.png"}
-                className='rounded-md object-cover'
+                className='rounded-sm object-cover'
                 fill
                 sizes="(min-width: 60em) 24vw,
                             (min-width: 28em) 45vw,
@@ -42,7 +55,7 @@ function Signin() {
           </div>
 
           <div className=''>
-            <div className='relative w-full mx-auto h-[240px]  rounded-sm'>
+            <div className='relative w-full mx-auto h-[220px]  rounded-sm'>
               <Image
                 alt=''
                 priority
@@ -55,16 +68,16 @@ function Signin() {
               />
             </div>
 
-            <form onSubmit={e => e.preventDefault()} action="" className='grid grid-cols-1 gap-y-4 '>
+            <form onSubmit={e => e.preventDefault()} action="" className='grid grid-cols-1 gap-y-4 -mt-5'>
               {/* Email */}
-              <div className='bg-background px-4 space-x-1 py-[2pxa] rounded-2xl flex justify-center items-center '>
+              <div className='bg-background px-4 space-x-1 py-[2px] rounded-2xl flex justify-center items-center '>
 
                 <FiMail className='h-6 w-6 text-gray-800 cursor-pointer hover:text-gray-400 transition-all ease-in-out duration-300' />
                 <input
                   placeholder='Email'
                   value={values.email}
                   onChange={handleChange}
-                  className='rounded-3xl text-gray-700 h-14 focus:ring-[#CAF0F8] border-[#CAF0F8] w-full bg-background focus:border-[#CAF0F8]'
+                  className='rounded-3xl text-gray-700 h-12 focus:ring-[#CAF0F8] border-[#CAF0F8] w-full bg-background focus:border-[#CAF0F8]'
                   type="email"
                   name="email" />
               </div>
@@ -87,13 +100,13 @@ function Signin() {
                 <input
                   placeholder='Password'
                   value={values.password}
-                  onChange={handleChange} className='rounded-3xl text-gray-700 h-14 focus:ring-[#CAF0F8] border-[#CAF0F8] w-full bg-background focus:border-[#CAF0F8]'
+                  onChange={handleChange} className='rounded-3xl text-gray-700 h-12 focus:ring-[#CAF0F8] border-[#CAF0F8] w-full bg-background focus:border-[#CAF0F8]'
                   type={showpass ? 'text' : 'password'}
                   name="password" />
               </div>
 
               {/* submit Button */}
-              <div className='flex justify-between px-1'>
+              <div className='flex justify-between'>
                 <div className="flex items-center">
                   <label className="flex items-center cursor-pointer">
                     <div className="relative">
@@ -101,12 +114,12 @@ function Signin() {
                       <div className="block transition-all ease-in-out duration-500 bg-[#CCCCCC] w-16 h-9 rounded-full"></div>
                       <div className="dot absolute left-1 top-1 bg-white w-7 h-7 rounded-full transition-all ease-in-out duration-500"></div>
                     </div>
-                    <div className="ml-3">
+                    <div className="text-[#676B6B] font-medium mx-1 text-sm">
                       Remember Password
                     </div>
                   </label>
                 </div>
-                <button className=' text-[#429291] ml-2 '  > Forget Password ?</button>
+                <button className=' text-secondary-text/60 font-medium'  > Forget Password ?</button>
               </div>
 
               <button className='w-full p-1 h-11 mt-4 rounded-3xl  transition-all duration-500 ease-in-out text-xl font-light ' onClick={loginUser} >Login</button>
