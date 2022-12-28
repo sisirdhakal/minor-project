c24f30311873bd0b47a53bc1760cdc60cad942c5
sisirdhakal <dhakalsaurav023@gmail.com>
2022-12-28 15:38:48 +0545

    footer done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
+// import { Provider } from 'react-redux'
+// import store from '../redux/store'
+import Footer from '../components/footer'
+import { useRouter } from 'next/router'
+import NextJSProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  return <>
+    {/* <Provider store={store}> */}
+    <div key={router.pathname} className='h-screen grid grid-rows-nav'>
+      <NextJSProgress
+        color="cyan"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <div className='grid grid-rows-auto relative'>
+        <Component {...pageProps} />
+        <div className='bottom-0 absolute w-full'>
+          <Footer />
+        </div>
+      </div>
+    </div>
+    {/* </Provider> */}
+  </>
 }
 
 export default MyApp
