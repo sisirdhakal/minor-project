65ce5f9cc66795a2cb151dfb74f98f6af34effed
sisirdhakal <dhakalsaurav023@gmail.com>
2022-12-31 19:05:04 +0545

    redux setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-// import { Provider } from 'react-redux'
-// import store from '../redux/store'
+import { Provider } from 'react-redux'
+import store from '../redux/store'
 import Footer from '../components/footer'
 import { useRouter } from 'next/router'
 import NextJSProgress from 'nextjs-progressbar';
@@ -9,23 +9,23 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return <>
-    {/* <Provider store={store}> */}
-    <div key={router.pathname} className='h-screen grid grid-rows-nav'>
-      <NextJSProgress
-        color="cyan"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-        showOnShallow={true}
-      />
-      <div className='grid grid-rows-auto relative'>
-        <Component {...pageProps} />
-        <div className='bottom-0 absolute w-full'>
-          <Footer />
+    <Provider store={store}>
+      <div key={router.pathname} className='h-screen grid grid-rows-nav'>
+        <NextJSProgress
+          color="cyan"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
+        <div className='grid grid-rows-auto relative'>
+          <Component {...pageProps} />
+          <div className='bottom-0 absolute w-full'>
+            <Footer />
+          </div>
         </div>
       </div>
-    </div>
-    {/* </Provider> */}
+    </Provider>
   </>
 }
 
