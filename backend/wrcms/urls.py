9a6c004afff4753b76f093859714b58410f83f5f
Sandesh Paudel <sandeshpaudel81@gmail.com>
2023-01-13 12:20:11 +0545

    working on parent verification

--- backend/wrcms/urls.py
+++ backend/wrcms/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GetCSRFToken
+from .views import GetCSRFToken, ParentVerification
 
 urlpatterns = [
     path('get-csrf/', GetCSRFToken.as_view()),
+    path('parent-verify/', ParentVerification.as_view()),
 ]
