caeafc9a55b1d973b97bd4d42b0ed0f4e0880b9f
Sandesh Paudel <sandeshpaudel81@gmail.com>
2023-02-02 01:23:22 +0545

    signup

--- backend/wrcms/urls.py
+++ backend/wrcms/urls.py
@@ -4,8 +4,7 @@ from .views import *
 urlpatterns = [
     path('check-auth/', CheckAuthenticated.as_view()),
     path('get-csrf/', GetCSRFToken.as_view()),
-    path('parent-verify/', ParentVerification.as_view()),
-    path('parent-signup/', ParentSignUp.as_view()),
-    path('teacher-student-verify/', TeacherStudentVerification.as_view()),
+    path('verify/', Verification.as_view()),
+    path('signup/', SignUp.as_view()),
     path('login/', LoginView.as_view()),
 ]
--- backend/wrcms/views.py
+++ backend/wrcms/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from .models import UserProfile, Student, UserRole, Parent
+from .models import UserProfile, Student, UserRole, Parent, Teacher
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 from django.utils.decorators import method_decorator
 from django.contrib.auth import authenticate, login
@@ -28,117 +28,158 @@ class CheckAuthenticated(APIView):
 
 
 @method_decorator(csrf_protect, name='dispatch')
-class ParentVerification(APIView):
+class Verification(APIView):
     permission_classes = (permissions.AllowAny, )
 
     def post(self, request, format=None):
         data = request.data
-        studentName = data['text']
+        nameOrID = data['text']
         idType = data['idType']
         idNumber = data['idNumber']
         dobStudent = data['dobStudent']
-        splitStudentName = studentName.split(" ")
-        role = UserRole.objects.get(type='Student')
-        try:
-            if (len(splitStudentName) == 3):
+        role = data['role']
+        if role=='two':
+            try:
+                splitStudentName = nameOrID.split(" ")
+                role = UserRole.objects.get(type='Student')
+                if (len(splitStudentName) == 3):
+                    userProfile = UserProfile.objects.get(
+                        firstName__iexact=splitStudentName[0],
+                        middleName__iexact=splitStudentName[1],
+                        lastName__iexact=splitStudentName[2],
+                        identificationDocumentType = idType,
+                        identificationDocumentNumber = idNumber,
+                        dateOfBirth = dobStudent
+                    )
+                    if (userProfile is not None and userProfile.role==role):
+                        student = Student.objects.get(userProfile=userProfile)
+                        if (student.isParentRegistered == True):
+                            return Response({'msg': 'Parents already registered'}, status=status.HTTP_409_CONFLICT)
+                        else:
+                            return Response({'msg': 'Student Verified', 'student': student.id}, status=status.HTTP_200_OK)
+                    else:
+                        return Response({'msg': 'No student found with given details'}, status=status.HTTP_404_NOT_FOUND)
+                else:
+                    userProfile = UserProfile.objects.get(
+                        firstName__iexact=splitStudentName[0],
+                        lastName__iexact=splitStudentName[1],
+                        identificationDocumentType = idType,
+                        identificationDocumentNumber = idNumber,
+                        dateOfBirth = dobStudent
+                    )
+                    if (userProfile is not None and userProfile.role==role):
+                        student = Student.objects.get(userProfile=userProfile)
+                        if (student.isParentRegistered == True):
+                            return Response({'msg': 'Parents already registered'}, status=status.HTTP_409_CONFLICT)
+                        else:
+                            return Response({'msg': 'Student Verified', 'student': student.id}, status=status.HTTP_200_OK)
+                    else:
+                        return Response({'msg': 'No student found with given details'}, status=status.HTTP_404_NOT_FOUND)
+            except:
+                return Response({'msg': 'No student found with given details'}, status=status.HTTP_404_NOT_FOUND)
+        elif role=='one':
+            try:
                 userProfile = UserProfile.objects.get(
-                    firstName__iexact=splitStudentName[0],
-                    middleName__iexact=splitStudentName[1],
-                    lastName__iexact=splitStudentName[2],
+                    portalId = nameOrID,
                     identificationDocumentType = idType,
                     identificationDocumentNumber = idNumber,
                     dateOfBirth = dobStudent
                 )
-                if (userProfile is not None and userProfile.role==role):
-                    student = Student.objects.get(userProfile=userProfile)
-                    if (student.isParentRegistered == True):
-                        return Response({'msg': 'Parents already registered'})
-                    else:
-                        return Response({'msg': 'Student Verified', 'student': student.id})
-                else:
-                    return Response({'msg': 'No student found with given details'})
-            else:
-                userProfile = UserProfile.objects.get(
-                    firstName__iexact=splitStudentName[0],
-                    lastName__iexact=splitStudentName[1],
-                    identificationDocumentType = idType,
-                    identificationDocumentNumber = idNumber,
-                    dateOfBirth = dobStudent
-                )
-                if (userProfile is not None and userProfile.role==role):
-                    student = Student.objects.get(userProfile=userProfile)
-                    if (student.isParentRegistered == True):
-                        return Response({'msg': 'Parents already registered'})
-                    else:
-                        return Response({'msg': 'Student Verified', 'student': student.id})
-                else:
-                    return Response({'msg': 'No student found with given details'})
-        except:
-            return Response({'msg': 'No student found with given details'})
+                if (userProfile is not None):
+                    return Response({'msg': 'User verified', 'userRole': userProfile.role.type, 'portalId':nameOrID}, status=status.HTTP_200_OK)
+            except:
+                return Response({'msg': 'No user found with given details.'}, status=status.HTTP_404_NOT_FOUND)
+        
 
 
 @method_decorator(csrf_protect, name='dispatch')
-class ParentSignUp(APIView):
+class SignUp(APIView):
     permission_classes = (permissions.AllowAny, )
 
     def post(self, request, format=None):
         data = request.data
-        courtesyTitle = data['courtesyTitle']
-        parentName = data['parentName']
-        address = data['address']
-        contactNumber = data['contactNumber']
-        email = data['email']
+        role = data['role']
         password = data['password']
         confirmPassword = data['confirmPassword']
-        studentId = data['id']
-
-        splitParentName = parentName.split(" ")
-        student = Student.objects.get(id=studentId)
-        if password == confirmPassword:
+        if role=='two':
             if User.objects.filter(username=email).exists():
-                return Response({'msg': 'Email is already registered.'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'msg': 'Email is already registered.'}, status=status.HTTP_409_CONFLICT)
             else:
-                user = User.objects.create_user(username=email, email=email, password=password)
-                user.save()
-                if (len(splitParentName) == 3):
-                    userProfile = UserProfile.objects.create(
-                        user = user,
-                        courtesyTitle = courtesyTitle,
-                        firstName = splitParentName[0],
-                        middleName = splitParentName[1],
-                        lastName = splitParentName[2],
-                        address = address,
-                        contact = contactNumber,
-                        role = UserRole.objects.get(type='Parent')
-                    )
-                    Parent.objects.create(
-                        user = user,
-                        userProfile = userProfile,
-                        parentOf = student
-                    )
-                    student.isParentRegistered = True
-                    student.save()
-                    return Response({'msg': 'Signed up successfully.'}, status=status.HTTP_200_OK)
-                else:
-                    userProfile = UserProfile.objects.create(
-                        user = user,
-                        courtesyTitle = courtesyTitle,
-                        firstName = splitParentName[0],
-                        lastName = splitParentName[1],
-                        address = address,
-                        contact = contactNumber,
-                        role = UserRole.objects.get(type='Parent')
-                    )
-                    Parent.objects.create(
-                        user = user,
-                        userProfile = userProfile,
-                        parentOf = student
-                    )
-                    student.isParentRegistered = True
-                    student.save()
-                    return Response({'msg': 'Signed up successfully.'}, status=status.HTTP_200_OK)
-        else:
-            return Response({'msg': 'Passwords do not match'}, status=status.HTTP_400_BAD_REQUEST)
+                courtesyTitle = data['courtesyTitle']
+                parentName = data['parentName']
+                address = data['address']
+                contactNumber = data['contactNumber']
+                email = data['email']
+                studentId = data['id']
+                student = Student.objects.get(id=studentId)
+                splitParentName = parentName.split(" ")
+                try:
+                    user = User.objects.create_user(username=email, email=email, password=password)
+                    user.save()
+                    if (len(splitParentName) == 3):
+                        userProfile = UserProfile.objects.create(
+                            user = user,
+                            courtesyTitle = courtesyTitle,
+                            firstName = splitParentName[0],
+                            middleName = splitParentName[1],
+                            lastName = splitParentName[2],
+                            address = address,
+                            contact = contactNumber,
+                            role = UserRole.objects.get(type='Parent')
+                        )
+                        Parent.objects.create(
+                            user = user,
+                            userProfile = userProfile,
+                            parentOf = student
+                        )
+                        student.isParentRegistered = True
+                        student.save()
+                        return Response({'msg': 'Signed up successfully.'}, status=status.HTTP_200_OK)
+                    else:
+                        userProfile = UserProfile.objects.create(
+                            user = user,
+                            courtesyTitle = courtesyTitle,
+                            firstName = splitParentName[0],
+                            lastName = splitParentName[1],
+                            address = address,
+                            contact = contactNumber,
+                            role = UserRole.objects.get(type='Parent')
+                        )
+                        Parent.objects.create(
+                            user = user,
+                            userProfile = userProfile,
+                            parentOf = student
+                        )
+                        student.isParentRegistered = True
+                        student.save()
+                        return Response({'msg': 'Signed up successfully.'}, status=status.HTTP_200_OK)
+                except:
+                    return Response({'msg': 'Error while signing up!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        elif role=='one':
+            userType = data['userRole']
+            portalID = data['portalId']
+            if userType=='Teacher':
+                try:
+                    role = UserRole.objects.get(type='Teacher')
+                    teacherUserProfile = UserProfile.objects.get(portalID=portalID, role=role)
+                    user = User.objects.create_user(username=teacherUserProfile.email, email=teacherUserProfile.email, password=password)
+                    user.save()
+                    teacherUserProfile.user = user
+                    teacher = Teacher.objects.get(userProfile=teacherUserProfile)
+                    teacher.user = user
+                except:
+                    return Response({'msg': 'Error while signing up!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            elif userType=='Student':
+                try:
+                    role = UserRole.objects.get(type='Student')
+                    studentUserProfile = UserProfile.objects.get(portalID=portalID, role=role)
+                    user = User.objects.create_user(username=studentUserProfile.email, email=studentUserProfile.email, password=password)
+                    user.save()
+                    studentUserProfile.user = user
+                    teacher = Teacher.objects.get(userProfile=studentUserProfile)
+                    teacher.user = user
+                except:
+                    return Response({'msg': 'Error while signing up!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @method_decorator(csrf_protect, name='dispatch')
@@ -160,26 +201,3 @@ class LoginView(APIView):
             return Response({'msg': 'User logged in successfully!', 'username': username, 'role': profile.role.type}, status=status.HTTP_200_OK)
         else:
             return Response({'msg': 'Incorrect password!'}, status=status.HTTP_400_BAD_REQUEST)
-
-    
-@method_decorator(csrf_protect, name='dispatch')
-class TeacherStudentVerification(APIView):
-    permission_classes = (permissions.AllowAny, )
-
-    def post(self, request, format=None):
-        data = request.data
-        portalId = data['text']
-        idType = data['idType']
-        idNumber = data['idNumber']
-        dob = data['dob']
-        try:
-            userProfile = UserProfile.objects.get(
-                portalId = portalId,
-                identificationDocumentType = idType,
-                identificationDocumentNumber = idNumber,
-                dateOfBirth = dob
-            )
-            if (userProfile is not None):
-                return Response({'msg': 'User verified', 'userRole': userProfile.role.type}, status=status.HTTP_200_OK)
-        except:
-            return Response({'msg': 'No user found with given details.'}, status=status.HTTP_404_NOT_FOUND)
