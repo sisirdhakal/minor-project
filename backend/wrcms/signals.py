6ab63c46eef6d6374a9ffc683282d3d966dc4f58
Sandesh Paudel <sandeshpaudel81@gmail.com>
2023-02-06 15:54:26 +0545

    email sent to student

--- backend/wrcms/signals.py
+++ backend/wrcms/signals.py
@@ -7,21 +7,22 @@ from django.conf import settings
 
 
 @receiver(post_save, sender=Student)
-def send_portal_invitation(sender, instance, **kwargs):
+def send_portal_invitation(sender, instance, created, **kwargs):
     """
     Send email to customer with order details.
     """
-    student = instance
-    userprofile = student.userProfile
-    message = get_template("invitation.html").render({
-        'userProfile': userprofile
-    })
-    mail = EmailMessage(
-        subject="Invitation: Signup For WRCMS",
-        body=message,
-        from_email=settings.EMAIL_HOST_USER,
-        to=[userprofile.email],
-        reply_to=[settings.EMAIL_HOST_USER],
-    )
-    mail.content_subtype = "html"
-    return mail.send()
+    if created:
+        student = instance
+        userprofile = student.userProfile
+        message = get_template("invitation.html").render({
+            'userProfile': userprofile
+        })
+        mail = EmailMessage(
+            subject="Invitation: Signup For WRCMS",
+            body=message,
+            from_email=settings.EMAIL_HOST_USER,
+            to=[userprofile.email],
+            reply_to=[settings.EMAIL_HOST_USER],
+        )
+        mail.content_subtype = "html"
+        return mail.send()
