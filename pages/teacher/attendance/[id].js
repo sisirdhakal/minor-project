9b32d07f122d4c6940247aeb53f049c63a4c83d4
sisirdhakal <dhakalsaurav023@gmail.com>
2023-02-17 23:56:55 +0545

    date added

--- pages/teacher/attendance/[id].js
+++ pages/teacher/attendance/[id].js
@@ -8,6 +8,7 @@ function AttendanceComp({ students }) {
         <div className=''>
             {/* <div className="h-auto bg-white  w-full items-center " > */}
             <div className='relative bg-white px-4 py-2 w-full h-full rounded-sm'>
+
                 <h1 className='text-primary-text text-lg mb-3 font-semibold capitalize'>department_name</h1>
                 <div className=' items-center flex'>
                     <h1 className='text-clrgrey1 mb-1 font-bold text-lg'>Class : <span >class_name</span></h1>
@@ -17,7 +18,11 @@ function AttendanceComp({ students }) {
                 </div>
             </div>
             <div className='h-full py-5'>
-                <div className='bg-white h-full py-3 px-4'>
+
+                <div className='bg-white h-full py-3 px-8'>
+                    <div className='mb-2 flex justify-end'>
+                        <p className='text-primary-text text-lg font-semibold'> Date : {new Date().toLocaleDateString('en-GB').split('/').reverse().join('/')}</p>
+                    </div>
                     <div>
                         <div className="grid grid-cols-3 mb-3">
                             <div className='px-4'>
