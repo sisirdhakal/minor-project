c74358f1b779c7800dc07496bacdc24e83007d31
sisirdhakal <dhakalsaurav023@gmail.com>
2023-02-17 22:29:43 +0545

    dynamic buttons done

--- pages/teacher/attendance/index.js
+++ pages/teacher/attendance/index.js
@@ -2,27 +2,36 @@ import axios from 'axios';
 import Link from 'next/link';
 import React from 'react'
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { DashboardLayout } from '../../../components/layout/dashboard';
 import LecturesComp from '../../../components/lectures';
+import { actionCreators } from '../../../redux';
 
-function TeacherAttendance({ lectures }) {
+function TeacherAttendance({ theory, practical }) {
 
-    console.log(lectures)
+    const dispatch = useDispatch()
+    const { setAttendanceType } = bindActionCreators(actionCreators, dispatch)
+
+    const { attendanceType } = useSelector(state => state.attendance)
 
     return (
         <>
             <div>
                 <div className='grid grid-cols-2 gap-32'>
-                    <button className='bg-white rounded-lg py-2 items-center shadow-md shadow-green-500 text-start flex px-5'>
+                    <button className={`bg-white rounded-lg py-2 items-center ${attendanceType === "th" ? "shadow-md shadow-green-500" : ""} text-start flex px-5`} onClick={() => { setAttendanceType("th") }}>
                         <p className='text-primary-text font-bold text-[1rem] my-auto '>Theory Lectures</p>
                     </button>
-                    <button className='bg-white rounded-lg py-2 text-start items-center px-5'>
+                    <button className={`bg-white rounded-lg py-2 text-start items-center px-5 ${attendanceType === "pr" ? "shadow-md shadow-green-500" : ""}`} onClick={() => { setAttendanceType("pr") }}>
                         <p className='text-primary-text my-auto font-bold text-[1rem]'>Practical Labs</p>
                     </button>
                 </div>
                 <div className='py-5'>
                     {
-                        <LecturesComp lectures={lectures} />
+                        attendanceType === "th" ?
+                            (<LecturesComp lectures={theory} />) : (
+                                <LecturesComp lectures={practical} />
+                            )
                     }
                 </div>
             </div>
@@ -34,7 +43,7 @@ export default TeacherAttendance
 
 export const getServerSideProps = async ({ req }) => {
 
-    let lectures = []
+    let theory = []
     try {
         const { data } = await axios.get("http://localhost:8000/api/get-lectures/", {
             withCredentials: true,
@@ -42,14 +51,15 @@ export const getServerSideProps = async ({ req }) => {
                 Cookie: req.headers.cookie
             }
         })
-        lectures = data;
+        theory = data;
     } catch (error) {
         console.log(error)
     }
 
     return {
         props: {
-            lectures: lectures || []
+            theory: theory || [],
+            practical: []
         }
     };
 }
--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import attendance_reducer from "./attendance";
 import auth_reducer from "./auth";
 import dashboard_reducer from "./dashboard";
 import signup_reducer from "./signup";
@@ -6,7 +7,8 @@ import signup_reducer from "./signup";
 const reducers = combineReducers({
     auth: auth_reducer,
     dashboard: dashboard_reducer,
-    signup: signup_reducer
+    signup: signup_reducer,
+    attendance: attendance_reducer
 })
 
 export default reducers;
