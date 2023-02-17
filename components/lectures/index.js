dc13950019587d45baf94a1d3fc346a019f05424
sisirdhakal <dhakalsaurav023@gmail.com>
2023-02-17 22:53:37 +0545

    dynamic name

--- components/lectures/index.js
+++ components/lectures/index.js
@@ -7,10 +7,10 @@ function LecturesComp({ lectures }) {
             <div>
                 {
                     lectures.map(item => {
-                        const { id, cLass, class_name, totalLectureDays, subject_name } = item
+                        const { id, class_name, totalLectureDays, subject_name, department_name } = item
                         return <div key={id} className="h-auto grid bg-white rounded-sm w-full items-center px-4 py-2" >
                             <div className='relative w-full h-full rounded-sm'>
-                                <h1 className='text-primary-text text-lg mb-1 font-semibold capitalize'>Department of Electronics and Computer Engineering</h1>
+                                <h1 className='text-primary-text text-lg mb-1 font-semibold capitalize'>{department_name}</h1>
                                 <div className='grid grid-cols-2'>
                                     <div className=' items-center flex'>
                                         <h1 className='text-clrgrey1 mb-3 font-bold text-lg'>Class : <span >{class_name}</span></h1>
