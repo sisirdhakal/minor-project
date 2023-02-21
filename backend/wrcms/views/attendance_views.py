580743c3a64faddb0b4f05ab8b8004e3beebb54d
Sandesh Paudel <sandeshpaudel81@gmail.com>
2023-02-21 23:29:43 +0545

    organized urls and views

--- backend/wrcms/views/attendance_views.py
+++ backend/wrcms/views/attendance_views.py
@@ -15,7 +15,6 @@ class GetLectures(APIView):
     permission_classes = (permissions.IsAuthenticated, )
 
     def get(self, request, format=None):
-        print(request.headers)
         user = request.user
         userProfile = UserProfile.objects.get(user=user)
         if userProfile.role.type == "Teacher":
@@ -48,24 +47,6 @@ class GetPracticalClass(APIView):
         else:
             return Response({'msg': 'Unauthorized access!'}, status=status.HTTP_401_UNAUTHORIZED)
 
-
-# @method_decorator(csrf_protect, name='dispatch')
-# class LectureDetailForAttendance(APIView):
-#     permission_classes = (permissions.IsAuthenticated, )
-
-#     def get(self, request, id, format=None):
-#         user = request.user
-#         try:
-#             userProfile = UserProfile.objects.get(user=user)
-#             requestedTeacher = Teacher.objects.get(user=user, userProfile=userProfile)
-#             lecture = Lecture.objects.get(id=id)
-#             if userProfile.role.type == "Teacher" and lecture.teacher==requestedTeacher:
-#                 serializer = LectureDetailSerializer(lecture, many=False)
-#                 return Response(serializer.data, status=status.HTTP_200_OK)
-#             else:
-#                 return Response({'msg': 'Unauthorized access!'}, status=status.HTTP_401_UNAUTHORIZED)
-#         except:
-#             return Response({'msg': 'Lecture unavailable!'}, status=status.HTTP_404_NOT_FOUND)
 
 
 @method_decorator(csrf_protect, name='dispatch')
