bd4ea17a8dad0fe4e8d44afe52d30c8194a19d66
Sandesh Paudel <sandeshpaudel81@gmail.com>
2023-05-08 18:03:12 +0545

    started leaveApplication

---
+++ backend/leaveApplication/views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
+from rest_framework import status
