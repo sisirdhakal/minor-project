a45ec5b593c5654acb4b6d08fb6f73c93de2211a
Sandesh Paudel <sandeshpaudel81@gmail.com>
2023-05-09 13:12:36 +0545

    request a leave completed

--- backend/leaveApplication/urls.py
+++ backend/leaveApplication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('add/', RequestLeave.as_view()),
 ]
--- backend/leaveApplication/views.py
+++ backend/leaveApplication/views.py
@@ -4,3 +4,45 @@ from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_protect
 from django.utils.decorators import method_decorator
 from rest_framework import status
+from wrcms.models import UserProfile, Student, Lecture
+from .models import LeaveRequest
+
+@method_decorator(csrf_protect, name='dispatch')
+class RequestLeave(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request, format=None):
+        user = request.user
+        data = self.request.data
+        leaveFrom = data['leaveFrom']
+        leaveTo = data['leaveTo']
+        lectureId = data['lecture']
+        reason = data['reason']
+        userProfile = UserProfile.objects.get(user=user)
+        if userProfile.role.type == 'Student':
+            student = Student.objects.get(user=user, userProfile=userProfile)
+            try:
+                lecture = Lecture.objects.get(id=lectureId, cLass=student.cLass)
+                if(lecture.cLass != student.cLass):
+                    return Response({'msg': 'Inappropriate relation between student and lecture.'}, status=status.HTTP_400_BAD_REQUEST)
+                LeaveRequest.objects.create(
+                    leaveStartDate=leaveFrom,
+                    leaveEndDate = leaveTo,
+                    student = student,
+                    lecture = lecture,
+                    reason = reason
+                )
+                return Response({'msg': 'Leave request submitted successfully.'}, status=status.HTTP_200_OK)
+            except:
+                return Response({'msg': 'Lecture not found.'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({'msg': 'No privilege to request leave.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+@method_decorator(csrf_protect, name='dispatch')
+class RequestLeave(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request, format=None):
+        user = request.user
+        data = self.request.data
