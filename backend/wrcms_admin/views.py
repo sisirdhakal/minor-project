f33efcecad8d2b46f801c72beed2901251f37bac
Sandesh Paudel <sandeshpaudel81@gmail.com>
2023-05-10 23:52:55 +0545

    wrcms admin batch and department completed

--- backend/wrcms_admin/views.py
+++ backend/wrcms_admin/views.py
@@ -9,6 +9,8 @@ import datetime
 from wrcms.models import Batch, Department, Program, Class, Lecture, Subject, Student, Teacher
 from .serializers import *
 
+# Class based views for CRUD operations of """Batch""" model
+
 @method_decorator(csrf_protect, name='dispatch')
 class BatchList(APIView):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
@@ -17,6 +19,112 @@ class BatchList(APIView):
         batches = Batch.objects.filter(graduated=False)
         serializer = BatchSerializer(batches, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+@method_decorator(csrf_protect, name='dispatch')
+class BatchAdd(APIView):
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
+    
+    def post(self, request, format=None):
+        data = self.request.data
+        try:
+            Batch.objects.create(
+                year = data['year'],
+                startedFrom = data['startedFrom']
+            )
+            return Response({'msg': 'Batch added successfully.'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'msg': 'Error while adding new batch.'}, status=status.HTTP_409_CONFLICT)
+        
+@method_decorator(csrf_protect, name='dispatch')
+class BatchEdit(APIView):
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
+    
+    def put(self, request, id, format=None):
+        data = self.request.data
+        try:
+            batch = Batch.objects.get(id=id)
+            batch.year = data['year']
+            batch.startedFrom = data['startedFrom']
+            batch.save()
+            return Response({'msg': 'Batch edited successfully.'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'msg': 'Batch not found.'}, status=status.HTTP_404_NOT_FOUND)
+    
+@method_decorator(csrf_protect, name='dispatch')
+class BatchDelete(APIView):
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
+    
+    def delete(self, request, id, format=None):
+        try:
+            batch = Batch.objects.get(id=id)
+            batch.delete()
+            return Response({'msg': 'Batch deleted successfully.'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'msg': 'Batch not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+# Class based views for CRUD operations of """Department""" model
+
+@method_decorator(csrf_protect, name='dispatch')
+class DepartmentList(APIView):
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
+
+    def get(self, request, format=None):
+        departments = Department.objects.all()
+        serializer = DepartmentSerializer(departments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+@method_decorator(csrf_protect, name='dispatch')
+class DepartmentAdd(APIView):
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
+    
+    def post(self, request, format=None):
+        data = self.request.data
+        try:
+            Department.objects.create(
+                name = data['name'],
+                description = data['description'],
+                contact = data['contact'],
+                mail = data['mail']
+            )
+            return Response({'msg': 'Department added successfully.'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'msg': 'Error while adding new department.'}, status=status.HTTP_409_CONFLICT)
+
+@method_decorator(csrf_protect, name='dispatch')
+class DepartmentEdit(APIView):
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
+    
+    def put(self, request, id, format=None):
+        data = self.request.data
+        try:
+            department = Department.objects.get(id=id)
+            department.name = data['name']
+            department.description = data['description']
+            department.contact = data['contact']
+            department.mail = data['mail']
+            if (data['hod']):
+                department.headOfDepartment = Teacher.objects.get(id=data['hod'])
+            if (data['dhod']):
+                department.deputyHeadOfDepartment = Teacher.objects.get(id=data['dhod'])
+            department.save()
+            return Response({'msg': 'Department edited successfully.'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'msg': 'Department not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+@method_decorator(csrf_protect, name='dispatch')
+class DepartmentDelete(APIView):
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
+    
+    def delete(self, request, id, format=None):
+        try:
+            department = Department.objects.get(id=id)
+            department.delete()
+            return Response({'msg': 'Department deleted successfully.'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'msg': 'Department not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+# Class based views for CRUD operations of """Class""" model
 
 @method_decorator(csrf_protect, name='dispatch')
 class ClassList(APIView):
@@ -34,14 +142,17 @@ class ClassAdd(APIView):
     
     def post(self, request, format=None):
         data = self.request.data
-        Class.objects.create(
-            name = data['name'],
-            department = Department.objects.get(id=data['department']),
-            batch = Batch.objects.get(id=data['batch']),
-            program = Program.objects.get(id=data['program']),
-            semester = data['semester']
-        )
-        return Response({'msg': 'Class added successfully.'}, status=status.HTTP_200_OK)
+        try:
+            Class.objects.create(
+                name = data['name'],
+                department = Department.objects.get(id=data['department']),
+                batch = Batch.objects.get(id=data['batch']),
+                program = Program.objects.get(id=data['program']),
+                semester = data['semester']
+            )
+            return Response({'msg': 'Class added successfully.'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'msg': 'Error while adding new class.'}, status=status.HTTP_409_CONFLICT)
     
 @method_decorator(csrf_protect, name='dispatch')
 class ClassEdit(APIView):
@@ -49,24 +160,30 @@ class ClassEdit(APIView):
     
     def put(self, request, id, format=None):
         data = self.request.data
-        cLass = Class.objects.get(id=id)
-        cLass.name = data['name']
-        cLass.department = Department.objects.get(id=data['department'])
-        cLass.batch = Batch.objects.get(id=data['batch'])
-        cLass.program = Program.objects.get(id=data['program'])
-        cLass.semester = data['semester']
-        if (data['cr']):
-            cLass.classRepresentative = Student.objects.get(id=data['cr'])
-        if (data['vcr']):
-            cLass.viceClassRepresentative = Student.objects.get(id=data['vcr'])
-        cLass.save()
-        return Response({'msg': 'Class edited successfully.'}, status=status.HTTP_200_OK)
+        try:
+            cLass = Class.objects.get(id=id)
+            cLass.name = data['name']
+            cLass.department = Department.objects.get(id=data['department'])
+            cLass.batch = Batch.objects.get(id=data['batch'])
+            cLass.program = Program.objects.get(id=data['program'])
+            cLass.semester = data['semester']
+            if (data['cr']):
+                cLass.classRepresentative = Student.objects.get(id=data['cr'])
+            if (data['vcr']):
+                cLass.viceClassRepresentative = Student.objects.get(id=data['vcr'])
+            cLass.save()
+            return Response({'msg': 'Class edited successfully.'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'msg': 'Class not found.'}, status=status.HTTP_404_NOT_FOUND)
     
 @method_decorator(csrf_protect, name='dispatch')
 class ClassDelete(APIView):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
     
     def delete(self, request, id, format=None):
-        cLass = Class.objects.get(id=id)
-        cLass.delete()
-        return Response({'msg': 'Class deleted successfully.'}, status=status.HTTP_200_OK)
+        try:
+            cLass = Class.objects.get(id=id)
+            cLass.delete()
+            return Response({'msg': 'Class deleted successfully.'}, status=status.HTTP_200_OK)
+        except:
+            return Response({'msg': 'Class not found.'}, status=status.HTTP_404_NOT_FOUND)
