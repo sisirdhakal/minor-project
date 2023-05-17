e995221cafeac9017f876f227e10b32b6ad6c9c6
sisirdhakal <dhakalsaurav023@gmail.com>
2023-05-17 23:12:12 +0545

    add department done

--- components/collgeadmin/department/editDepartment/index.js
+++ components/collgeadmin/department/editDepartment/index.js
@@ -6,38 +6,47 @@ import axios from 'axios'
 
 const EditDepartment = ({ cookie, id }) => {
 
-    const [process, setprocess] = useState("Edit Batch")
+    const [process, setprocess] = useState("Edit Department")
+    const [teachers, setTeachers] = useState([])
     const router = useRouter()
     const initialValue = {
-        year: "",
-        startedFrom: "",
-        graduated: false
+        name: "",
+        description: "",
+        mail: '',
+        contact: "",
+        hod: "",
+        dhod: "",
     }
 
 
     const [values, setvalues] = useState(initialValue)
 
     const handleChange = (e) => {
-        if (e.target.name === "graduated") {
-            setvalues({ ...values, [e.target.name]: e.target.checked })
-        }
-        else {
-            setvalues({ ...values, [e.target.name]: e.target.value })
-        }
+        setvalues({ ...values, [e.target.name]: e.target.value })
     }
 
 
     useEffect(() => {
         const getData = async () => {
             try {
-                const { data } = await axios.get(`http://localhost:8000/api/admin/batch/${id}/`, {
+                const { data } = await axios.get(`http://localhost:8000/api/admin/department/${id}/`, {
                     withCredentials: true,
                     headers: {
                         "X-CSRFTOKEN": cookie.csrftoken
                     }
                 })
                 if (data) {
-                    setvalues(data)
+                    console.log(data)
+                    const newObject = {
+                        "name": data.name ?? '',
+                        "description": data.description ?? '',
+                        "contact": data.contact ?? '',
+                        "mail": data.mail ?? '',
+                        "hod": data.headOfDepartment ?? '',
+                        "dhod": data.deputyHeadOfDepartment ?? ''
+                    }
+                    setvalues(newObject)
+
                 }
 
             } catch (error) {
@@ -49,20 +58,35 @@ const EditDepartment = ({ cookie, id }) => {
         }
         getData()
     }, [])
-    console.log(values)
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const { data } = await axios.get(`http://localhost:8000/api/admin/teacher/`, {
+                    withCredentials: true,
+                    headers: {
+                        "X-CSRFTOKEN": cookie.csrftoken
+                    }
+                })
+                if (data) {
+                    const filteredTeachers = data.filter(item => Number(item.department) === Number(id))
+                    setTeachers(filteredTeachers)
+                }
+
+            } catch (error) {
+                if (error) {
+                    console.log(error)
+                }
+            }
+
+        }
+        getData()
+    }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        if (values.year > 2100) {
-            return toast.error("Maximum year date is 2100 B.S")
-        }
-        if (values.year < 2058) {
-            return toast.error("Minimum year date is 2058 B.S")
-        }
-
         try {
             setprocess("Editing ...")
-            const { data } = await axios.put(`http://localhost:8000/api/admin/batch/${id}/edit/`, values, {
+            const { data } = await axios.put(`http://localhost:8000/api/admin/department/${id}/edit/`, values, {
                 withCredentials: true,
                 headers: {
                     "X-CSRFTOKEN": cookie.csrftoken
@@ -70,13 +94,12 @@ const EditDepartment = ({ cookie, id }) => {
             })
             if (data) {
                 toast.success(data.msg)
-                setprocess("Edit Batch")
-                router.push("/collegeadmin/batch")
+                setprocess("Edit Department")
+                router.push("/collegeadmin/department")
             }
 
         } catch (error) {
-            setprocess("Edit Batch")
-            console.log(error)
+            setprocess("Edit Department")
             if (error.response?.data.msg) {
                 toast.error(error.response.data.msg)
             }
@@ -88,56 +111,108 @@ const EditDepartment = ({ cookie, id }) => {
     return (
         <div>
             <div>
-                <CollegeAdminHero parent={"batch"} title={"Edit Batch"} image={"/assets/images/attendance.svg"} />
+                <CollegeAdminHero parent={"department"} title={"Edit Department"} image={"/assets/images/exam.svg"} />
             </div>
             <div>
-                <div className='h-full bg-white rounded-sm w-full px-8 py-6'>
+                <div className='h-full mb-6 bg-white rounded-sm w-full px-8 py-6'>
 
                     <div>
                         <h1 className='text-[#023E8A] mb-10 text-2xl font-medium'>
-                            Edit Batch
+                            Edit Department
                         </h1>
                     </div>
 
                     <form action="" className='w-full mt-6' onSubmit={handleSubmit}>
                         <div className='mb-4 flex justify-start items-center'>
-                            <p className='text-[#023E8A] text-xl w-32 font-medium mr-5'>Year</p>
+                            <p className='text-[#023E8A] text-xl w-32 font-medium mr-5'>Name</p>
                             <input
-                                value={values.year}
+                                value={values.name}
                                 onChange={handleChange}
                                 type="text"
-                                name='year'
-                                placeholder=''
+                                name='name'
+                                required
+                                placeholder='name'
+                                className='rounded text-gray-700 h-9 focus:ring-[#CAF0F8] border-[#CAF0F8] w-full max-w-[460px] bg-background focus:border-[#CAF0F8] placeholder:text-[#676B6B] placeholder:font-medium placeholder:tracking-wide' />
+                        </div>
+                        <div className='mb-4 flex justify-start'>
+                            <p className='text-[#023E8A] text-xl w-32 font-medium mr-5'>Description</p>
+                            <textarea
+                                value={values.description}
+                                onChange={handleChange}
+                                type="text"
+                                name='description'
+                                placeholder='Description of department'
+                                className='rounded text-gray-700 h-16 focus:ring-[#CAF0F8] border-[#CAF0F8] max-w-[460px] w-full bg-background focus:border-[#CAF0F8] placeholder:text-[#676B6B] placeholder:font-medium placeholder:tracking-wide' />
+                        </div>
+                        <div className='mb-4 flex justify-start items-center'>
+                            <p className='text-[#023E8A] text-xl w-32 font-medium mr-5'>Email</p>
+                            <input
+                                value={values.mail}
+                                onChange={handleChange}
+                                type="email"
+                                name='mail'
+                                placeholder='email'
+                                className='rounded text-gray-700 h-9 focus:ring-[#CAF0F8] border-[#CAF0F8] w-full max-w-[460px] bg-background focus:border-[#CAF0F8] placeholder:text-[#676B6B] placeholder:font-medium placeholder:tracking-wide' />
+                        </div>
+                        <div className='mb-4 flex justify-start items-center'>
+                            <p className='text-[#023E8A] text-xl w-32 font-medium mr-5'>Phone</p>
+                            <input
+                                value={values.contact}
+                                onChange={handleChange}
+                                type="number"
+                                name='contact'
+                                placeholder='phone'
+                                className='rounded text-gray-700 h-9 focus:ring-[#CAF0F8] border-[#CAF0F8] w-full max-w-[460px] bg-background focus:border-[#CAF0F8] placeholder:text-[#676B6B] placeholder:font-medium placeholder:tracking-wide' />
+                        </div>
+                        <div className='mb-4 flex justify-start items-center'>
+                            <p className='text-[#023E8A] text-xl w-32 font-medium mr-5'>HOD</p>
+                            <select
+                                className='bg-background w-full max-w-[460px] py-[0px] flex justify-center items-center h-[36px] border-0 rounded cursor-pointer text-[#676B6B] font-medium focus:ring-0'
+                                placeholder='hod'
+                                name='hod'
+                                value={values.hod}
+                                onChange={handleChange}
+                            >
+                                <option value="" disabled defaultValue>Teacher's Name</option>
+                                {
+                                    teachers?.map(item => {
+                                        const { user_profile } = item
+                                        if (values.dhod == item.id) return;
+                                        return <option key={item.id} value={item.id} className='cursor-pointer capitalize'>{user_profile.firstName} {user_profile.middleName} {user_profile.lastName}</option>
+                                    })
+                                }
 
-                                className='rounded text-gray-700 h-9 focus:ring-[#CAF0F8] border-[#CAF0F8] max-w-[140px] bg-background focus:border-[#CAF0F8] placeholder:text-[#676B6B] placeholder:font-medium placeholder:tracking-wide' />
+
+                            </select>
+
                         </div>
                         <div className='mb-4 flex justify-start items-center'>
-                            <p className='text-[#023E8A] text-xl w-32 font-medium mr-5'>Started From</p>
-                            <input
-                                value={values.startedFrom}
+                            <p className='text-[#023E8A] text-xl w-32 font-medium mr-5'>DHOD</p>
+                            <select
+                                className='bg-background w-full max-w-[460px] py-[0px] flex justify-center items-center h-[36px] border-0 rounded cursor-pointer text-[#676B6B] font-medium focus:ring-0'
+                                placeholder='hod'
+                                name='dhod'
+                                value={values.dhod}
                                 onChange={handleChange}
-                                type="text"
-                                name='startedFrom'
-                                placeholder=''
-                                className='rounded text-gray-700 h-9 focus:ring-[#CAF0F8] border-[#CAF0F8] max-w-[140px] bg-background focus:border-[#CAF0F8] placeholder:text-[#676B6B] placeholder:font-medium placeholder:tracking-wide' />
-                        </div>
-                        <div className='mb-4 flex justify-start items-center'>
-                            <div className='mr-8 cursor-pointer'>
-                                <input
-                                    onChange={handleChange}
-                                    type="checkbox"
-                                    value={values.graduated}
-                                    name='graduated'
-                                    placeholder=''
-                                    checked={values.graduated}
-                                    className='rounded text-gray-700 h-8 focus:ring-[#CAF0F8] border-[#CAF0F8] w-8 bg-background focus:border-[#CAF0F8] placeholder:text-[#676B6B] placeholder:font-medium cursor-pointer placeholder:tracking-wide' />
-                            </div>
-                            <p className='text-[#023E8A] text-xl w-32 font-medium mr-5'>Graduated</p>
+                            >
+                                <option value="" disabled defaultValue>Teacher's Name</option>
+                                {
+                                    teachers?.map(item => {
+                                        const { user_profile } = item
+                                        if (values.hod == item.id) {
+                                            return;
+                                        }
+                                        return <option key={item.id} value={item.id} className='cursor-pointer capitalize'>{user_profile.firstName} {user_profile.middleName} {user_profile.lastName}</option>
+                                    })
+                                }
+
+
+                            </select>
                         </div>
 
 
                         <div className='mt-12 mb-3 flex items-center justify-center'>
-                            <button disabled={process === "Edit Batch" ? false : true} className='bg-[#2091F9] rounded-lg hover: py-[4px] tracking-wider font-medium capitalize text-white text-[20px] px-3 text-clrprimary10 transition-all ease-linear duration-300 w-40 disabled:cursor-not-allowed'>
+                            <button disabled={process === "Edit Department" ? false : true} className='bg-[#2091F9] rounded-lg hover: py-[4px] tracking-wider font-medium capitalize text-white text-[20px] px-3 text-clrprimary10 transition-all ease-linear duration-300 w-56 disabled:cursor-not-allowed'>
                                 {process}
                             </button>
                         </div>
