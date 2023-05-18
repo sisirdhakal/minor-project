7b5cbb6c07a48908d7511cb6f97f097ae0752cca
Sandesh Paudel <sandeshpaudel81@gmail.com>
2023-05-18 10:57:45 +0545

    program UI

--- redux/reducers/collegeadmin.js
+++ redux/reducers/collegeadmin.js
@@ -1,9 +1,10 @@
 import { setUserDetails } from "../actionCreators";
-import { SET_ALL_BATCHES, SET_ALL_CLASSES, SET_ALL_DEPARTMENTS, SET_USER_DETAILS } from "../constant";
+import { SET_ALL_BATCHES, SET_ALL_CLASSES, SET_ALL_DEPARTMENTS, SET_ALL_PROGRAMS, SET_USER_DETAILS } from "../constant";
 
 const initialState = {
     allBatches: [],
     allDepartments: [],
+    allPrograms: [],
     allClasses: [],
     userDetails: null
 }
@@ -14,6 +15,9 @@ const collegeadmin_reducer = (state = initialState, action) => {
     }
     if (action.type === SET_ALL_DEPARTMENTS) {
         return { ...state, allDepartments: action.payload }
+    }
+    if (action.type === SET_ALL_PROGRAMS) {
+        return { ...state, allPrograms: action.payload }
     }
     if (action.type === SET_ALL_CLASSES) {
         return { ...state, allClasses: action.payload }
