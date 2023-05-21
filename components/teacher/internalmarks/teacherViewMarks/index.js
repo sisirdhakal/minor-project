7582269a4aa880355425309a5a0d82b58c76ac0c
Sandesh Paudel <sandeshpaudel81@gmail.com>
2023-05-21 12:11:59 +0545

    teacher view marks

--- components/teacher/internalmarks/teacherViewMarks/index.js
+++ components/teacher/internalmarks/teacherViewMarks/index.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import { toast } from "react-hot-toast"
+import CenteredLoading from "../../../../common/Loader"
 
-const ViewMarks = ({cookie, id}) => {
+const ViewMarks = ({cookies, id}) => {
     const [loading, setloading] = useState(false)
     const [marks, setmarks] = useState([])
 
@@ -13,16 +14,16 @@ const ViewMarks = ({cookie, id}) => {
                 const { data } = await axios.get(`http://localhost:8000/api/internal-marks/view/${id}/`, {
                     withCredentials: true,
                     headers: {
-                        "X-CSRFTOKEN": cookie.csrftoken
+                        "X-CSRFTOKEN": cookies.csrftoken
                     }
                 })
                 if (data) {
-                    console.log(data)
                     setloading(false)
                     setmarks(data)
                 }
 
             } catch (error) {
+                console.log(error)
                 if (error.response?.data.msg) {
                     setloading(false)
                     toast.error(error.response.data.msg)
@@ -35,27 +36,36 @@ const ViewMarks = ({cookie, id}) => {
     
     return (
         <div className='bg-white p-5 gap-y-3 rounded-md'>
-            <div className='grid grid-cols-7 font-semibold text-xl'>
-                <div className='col-span-3'>Student</div>
-                <div className='text-center'>Theory FM</div>
-                <div className='text-center'>Theory OM</div>
-                <div className='text-center'>Practical FM</div>
-                <div className='text-center'>Practical OM</div>
-            </div>
-            <div className='h-1 bg-slate-500'></div>
             {
-                marks?.students?.map(item => {
-                    const { id, full_name, internalMark:im } = item
-                    return <div key={id} className='grid grid-cols-7 my-3 text-lg'>
-                        <div className='col-span-3'>{full_name}</div>
-                        <div className='text-center'>{im.theoryFM}</div>
-                        <div className='text-center'>{im.theoryAssessment}</div>
-                        <div className='text-center'>{im.practicalFM}</div>
-                        <div className='text-center'>{im.practicalAssessment}</div>
-                        <div className='h-1 bg-slate-200 col-span-8'></div>
-                    </div>
-                    }
-                )
+                !loading ?
+            <div>
+                <div className='grid grid-cols-7 font-semibold text-xl'>
+                    <div className='col-span-3'>Student</div>
+                    <div className='text-center'>Theory FM</div>
+                    <div className='text-center'>Theory OM</div>
+                    <div className='text-center'>Practical FM</div>
+                    <div className='text-center'>Practical OM</div>
+                </div>
+                <div className='h-1 bg-slate-500'></div>
+                {
+                    marks?.students?.map(item => {
+                        const { id, full_name, internalMark:im } = item
+                        return <div key={id} className='grid grid-cols-7 my-3 text-lg'>
+                            <div className='col-span-3'>{full_name}</div>
+                            <div className='text-center'>{im.theoryFM}</div>
+                            <div className='text-center font-semibold'>{im.theoryAssessment}</div>
+                            <div className='text-center'>{im.practicalFM}</div>
+                            <div className='text-center font-semibold'>{im.practicalAssessment}</div>
+                            <div className='h-1 bg-slate-200 col-span-8'></div>
+                        </div>
+                        }
+                    )
+                }
+            </div> : 
+            <div className='lg:min-h-[460px] py-2 lg:pt-20 px-5 bg-white w-full rounded-sm'>
+                <p className='text-secondary-text text-center text-lg font-medium'>Loading marks...</p>
+                <CenteredLoading />
+            </div>
             }
         </div>
 )}
--- pages/parent/index.js
+++ pages/parent/index.js
@@ -5,6 +5,7 @@ import { statsOptions } from '../../utils/constants';
 import Link from 'next/link';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
+import CenteredLoading from '../../common/Loader';
 
 export default function Parent({cookie}) {
     const [dashboard, setdashboard] = useState(null)
@@ -40,25 +41,31 @@ export default function Parent({cookie}) {
     return (
         <div>
             <div className='grid lg:grid-cols-3 gap-6 mb-8'>
-
-                <div className="h-44 grid grid-cols-2 bg-white rounded-sm w-full items-center px-4" >
-                    <div className='relative w-[114px]  h-[140px] flex justify-center items-center rounded-sm'>
-                        <div className='border-[12px] rounded-full w-[114px] h-[114px] border-t-[#6DC9F7] rotate-[24deg] border-blue-400 bg-red-400 flex justify-center items-center'>
-                            <p className='text-xl font-bold -rotate-[24deg] text-white'>{dashboard?.presentPercentage} %</p>
+                {
+                    dashboardLoading ? 
+                    <div className='h-44 bg-white rounded-sm w-full items-center px-4 pt-10'>
+                        <CenteredLoading />
+                    </div>
+                    :
+                    <div className="h-44 grid grid-cols-2 bg-white rounded-sm w-full items-center px-4" >
+                        <div className='relative w-[114px]  h-[140px] flex justify-center items-center rounded-sm'>
+                            <div className='border-[12px] rounded-full w-[114px] h-[114px] border-t-[#6DC9F7] rotate-[24deg] border-blue-400 bg-red-400 flex justify-center items-center'>
+                                <p className='text-xl font-bold -rotate-[24deg] text-white'>{dashboard?.presentPercentage} %</p>
+                            </div>
+                        </div>
+                        <div >
+                            <h1 className='text-primary-text font-bold text-lg'>Attendance</h1>
+                            <p className='text-secondary-text font-medium'>This Semester</p>
+                            <p className='text-secondary-text font-medium'>Total Lectures: <span className='text-primary-text'>{dashboard?.lectureDays}</span></p>
+                            <p className='text-secondary-text font-medium'>Present: <span className='text-primary-text'>{dashboard?.presentDays}</span></p>
+                            <Link href='/teacher/attendance'>
+                                <button className='bg-[#2091F9] rounded-lg hover: py-[3px] tracking-wider font-medium text-white px-2 text-clrprimary10 transition-all ease-linear duration-300 hover:text-'>
+                                    View Details
+                                </button>
+                            </Link>
                         </div>
                     </div>
-                    <div >
-                        <h1 className='text-primary-text font-bold text-lg'>Attendance</h1>
-                        <p className='text-secondary-text font-medium'>This Semester</p>
-                        <p className='text-secondary-text font-medium'>Total Lectures: <span className='text-primary-text'>{dashboard?.lectureDays}</span></p>
-                        <p className='text-secondary-text font-medium'>Present: <span className='text-primary-text'>{dashboard?.presentDays}</span></p>
-                        <Link href='/teacher/attendance'>
-                            <button className='bg-[#2091F9] rounded-lg hover: py-[3px] tracking-wider font-medium text-white px-2 text-clrprimary10 transition-all ease-linear duration-300 hover:text-'>
-                                View Details
-                            </button>
-                        </Link>
-                    </div>
-                </div>
+                }
 
                 {
                     statsOptions.map(item => {
@@ -92,7 +99,17 @@ export default function Parent({cookie}) {
             </div>
         </div>
     )
+
 }
+
+export const getServerSideProps = async ({ req }) => {
+    return {
+        props: {
+            cookie: req.cookies
+        }
+    };
+}
+
 Parent.getLayout = function getLayout(page) {
     return <DashboardLayout>{page}</DashboardLayout>;
 };
